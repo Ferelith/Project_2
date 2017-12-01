dc0fe7b65785da9b0229a1b5c7e10313af34b994
Artur <artur.060969@gmail.com>
2017-12-01 11:57:34 +0000

    Created database & Implemented connection string
    Local database has been created. Implemented connection string to connect to database from the app. Requires a public method or class to access the connection string when sending queries.

---
+++ BugTrackerApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BugTrackerApp
+{
+    public class Connection
+    {
+        public Connection(string data)
+        {
+            // Initializing the connection.
+            SqlConnection mySqlConnection = new SqlConnection(data);
+            // Opening the connection using mySqlConnection and passed string.
+            mySqlConnection.Open();
+            // Checking connection state.
+            if (mySqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("Connected");
+            } else
+            {
+                MessageBox.Show("Not connected");
+            }
+        }
+
+        public void closeConn(string data)
+        {
+            
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public string databaseCon = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\c3469557\\Documents\\TrackerDB.mdf;Integrated Security = True; Connect Timeout = 30";
+        public Form1()
+        {
+            InitializeComponent();
+            // Passing a connection string to class Connection.
+            Connection conn = new Connection(databaseCon);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
