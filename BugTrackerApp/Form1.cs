7e915b7832e56206e5e4ea34e009174fc5d16a9a
Artur <artur.060969@gmail.com>
2017-12-01 12:42:05 +0000

    SqlConnection & Command are now accessible
    SqlConnection and Command are now accessible from methods and classes to send queries.

--- BugTrackerApp/Form1.cs
+++ BugTrackerApp/Form1.cs
@@ -11,14 +11,32 @@ using System.Data.SqlClient;
 
 namespace BugTrackerApp
 {
+    public partial class Form1 : Form
+    {
+        public static string databaseCon = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\c3469557\\Documents\\TrackerDB.mdf;Integrated Security = True; Connect Timeout = 30";
+        Connection conn = new Connection(databaseCon);
+        public Form1()
+        {
+            InitializeComponent();
+            // Passing a connection string to class Connection.
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
     public class Connection
     {
+        
+        SqlConnection mySqlConnection = new SqlConnection();
+        SqlCommand mySqlCommand = new SqlCommand();
         public Connection(string data)
         {
             // Initializing the connection.
             SqlConnection mySqlConnection = new SqlConnection(data);
             // Opening the connection using mySqlConnection and passed string.
             mySqlConnection.Open();
+            /*
             // Checking connection state.
             if (mySqlConnection.State == System.Data.ConnectionState.Open)
             {
@@ -27,25 +45,18 @@ namespace BugTrackerApp
             {
                 MessageBox.Show("Not connected");
             }
+            */
+        }
+        public void sendQuery()
+        {
+
         }
 
         public void closeConn(string data)
         {
-            
+            // Closing connection with the database.
+           
         }
     }
-    public partial class Form1 : Form
-    {
-        public string databaseCon = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\c3469557\\Documents\\TrackerDB.mdf;Integrated Security = True; Connect Timeout = 30";
-        public Form1()
-        {
-            InitializeComponent();
-            // Passing a connection string to class Connection.
-            Connection conn = new Connection(databaseCon);
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
-    }
+   
 }
